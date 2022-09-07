1324d6bf9cdbb4ab02785131752becf95009f43f
Patricio Bertini <patobertini10@gmail.com>
2022-09-07 11:06:02 +0200

    updating

--- src/Components/Projects/AboutMe.jsx
+++ src/Components/Projects/AboutMe.jsx
@@ -5,14 +5,13 @@ const AboutMe = () => {
         <h2>About Me</h2>
         <h5>
          
-          My name is Patricio Bertini, I am 28 years old, and I live in
-          Valencia, Spain. I have a degree in Economics and I am also a
+          My name is Patricio Bertini, I am 28 years old, living in Spain. I have a degree in Economics and I am also a
           professional Chef but I have been interested in Computing and in the
           IT area my entire life. This 2022 I decided to leave the comfort zone
           and decided to launch myself into this new career doing what I am
           passionate about, Programming. <br />
           Today I finished my studies as a
-          front-end developer. However, I keep on studying to become a Full
+          front-end developer. However, I keep on studying to become a MERN Full
           Stack Developer in the near future. I am seeking a new position as
           Junior Front-end, and to achieve this objective, I have created this portfolio to
           expose the projects I have carried out so far. Hope you enjoy it! <br />
